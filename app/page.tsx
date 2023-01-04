31c283b12a4e16c7d7e287ce664c3c2b27609443
saurabhan <saurabhan.001@gmail.com>
2023-01-04 22:07:18 +0530

    added Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Balancer from "react-wrap-balancer";
+import "./output.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      New website comes here
+      <Balancer>
+        <p className="text-5xl font-extrabold">
+          "Maybe we’ve spent too long trying to figure this out with theory.”
+        </p>
+      </Balancer>
     </div>
-  )
+  );
 }
