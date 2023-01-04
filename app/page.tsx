c0848d5082018bfe3bd80656e884235dc570edfa
saurabhan <saurabhan.001@gmail.com>
2023-01-04 22:38:41 +0530

    hero design

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import Balancer from "react-wrap-balancer";
-import "./output.css"
+import "./output.css";
+import blackhole from "../public/hero.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <Balancer>
-        <p className="text-5xl font-extrabold">
-          "Maybe we’ve spent too long trying to figure this out with theory.”
-        </p>
-      </Balancer>
+    <div className="container mx-auto relative">
+      <div className="text-center relative flex-col h-80 justify-center flex items-center">
+        <Balancer>
+          <p className="z-9 text-5xl font-extrabold text-amber-100">
+            "Maybe we’ve spent too long trying to figure this out with theory.”
+          </p>
+        </Balancer>
+      </div>
+      <div className="flex justify-center -mt-40">
+        <Image src={blackhole} alt="Picture of BlackHole" />
+      </div>
     </div>
   );
 }
